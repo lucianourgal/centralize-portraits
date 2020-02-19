e6342afd49a11526c65f203f112903535068843f
lucianourgal <lucianourgal@hotmail.com>
2020-02-18 22:42:16 -0300

    Working toy problem

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
  # Usefull links
  https://www.npmjs.com/package/faced <br>
  https://github.com/justadudewhohacks/face-api.js <br>
+ https://medium.com/github-all-stars/github-all-stars-2-face-api-js-f3d6f135f4f7 <br>
+ https://itnext.io/face-api-js-javascript-api-for-face-recognition-in-the-browser-with-tensorflow-js-bcc2a6c4cf07 <br>
+ https://medium.com/@muehler.v/node-js-face-recognition-js-simple-and-robust-face-recognition-using-deep-learning-ea5ba8e852 <br>
+
+
+# Notes
+face-recognition.js package doesnt work in node >= 12. Try node version 8 <br>
--- index.ts
+++ index.ts
@@ -1,9 +1,26 @@
+const fr = require('face-recognition')
+
+const centralizePicture = async (name) => {
+
+    const image = fr.loadImage(name)
+    const detector = fr.FaceDetector()
+    const targetSize = 150
+    const faceImages = detector.detectFaces(image, targetSize)
+    faceImages.forEach((img, i) => fr.saveImage(`face_${i}.png`, img))
+
+}
+
+centralizePicture('samples/2018.jpg');
+
+
+/*
 // import nodejs bindings to native tensorflow,
 // not required, but will speed up things drastically (python required)
 //import '@tensorflow/tfjs-node';
 import * as canvas from 'canvas';
 import * as faceapi from 'face-api.js';
 import { readFile } from 'fs'
+import { tf, toNetInput } from 'face-api.js';
 // implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
 
 function toArrayBuffer(buf) {
@@ -16,23 +33,25 @@ function toArrayBuffer(buf) {
 }
 
 
-const centralizePicture = (name: string) => {
 
+ /*
     // patch nodejs environment, we need to provide an implementation of
     // HTMLCanvasElement and HTMLImageElement
+    
     const { Canvas, Image, ImageData } = canvas;
-    faceapi.env.monkeyPatch({ /*Canvas, Image,*/ ImageData })
-
+    const x = faceapi.env.monkeyPatch({ /*Canvas, Image, ImageData })
+    
 
     readFile(name, function (err, data) {
         if (err) throw err;
 
         const img = toArrayBuffer(data)
-        const detection = faceapi.detectSingleFace(img) //What is TNetInput ????
+
+        const netInput = img instanceof tf.Tensor
+      ? img
+      : await toNetInput(data)
+
+        const detection = faceapi.detectSingleFace(img as Float32Array) //What is TNetInput ????
         console.log(detection);
         //res.write(data);
-    });
-
-}
-
-centralizePicture('samples/2018.jpg');
+    }); */
