60380bd8bbf013a05de8a271f1d8539e81dec728
lucianourgal <lucianourgal@hotmail.com>
2020-02-18 23:58:16 -0300

    Identifies recommended image cut

--- index.ts
+++ index.ts
@@ -1,57 +1,52 @@
-const fr = require('face-recognition')
+const fr = require('face-recognition');
+
+
+const cutAdvice = (faceRects, imageWidth) => {
+
+    const left =  faceRects[0].rect.left;
+    const right =  faceRects[0].rect.right;
+
+    const faceWidth = right - left;
+    let smallSize;
+    let smallSizeWidth;
+    let largeSizeWidth;
+
+    if(left < (imageWidth - right)) {
+        smallSize = 'LEFT';
+        smallSizeWidth = left;
+        largeSizeWidth = imageWidth - right;
+    } else {
+        smallSize = 'RIGHT';
+        smallSizeWidth = imageWidth - right;
+        largeSizeWidth = left;
+    }
+    const shouldCutSize = largeSizeWidth - smallSizeWidth;
+
+    console.log('[Width] Image has '+imageWidth+'px, Face has ' + faceWidth + 'px;');
+    console.log('[Width] SmallSize is '+smallSize+' with ' + smallSizeWidth + 'px against '+largeSizeWidth+'px of large size;');
+    const shouldCut = (smallSize === 'LEFT' ?  'RIGHT' : 'LEFT');
+    console.log('[Width] Recommend cut of ' + shouldCutSize+ 'px at the ' + shouldCut);
+
+    return { shouldCut, shouldCutSize };
+}
+
 
 const centralizePicture = async (name) => {
 
     const image = fr.loadImage(name)
     const detector = fr.FaceDetector()
-    const targetSize = 150
-    const faceImages = detector.detectFaces(image, targetSize)
-    faceImages.forEach((img, i) => fr.saveImage(`face_${i}.png`, img))
+    //const targetSize = 150
+    //const faceImages = detector.detectFaces(image, targetSize)
+    //faceImages.forEach((img, i) => fr.saveImage(`face_${i}.png`, img))
+
+    const faceRects = detector.locateFaces(image)
+    // faceRects.MmodRect.left: Pixel recognition starts,  faceRects.right: Pixel recognition ends
+    // [ MmodRect { confidence: 1.041917324066162,
+    //rect: Rect { area: 240590, bottom: 977, top: 488, right: 810, left: 320 } } ]
+
+    const advice = cutAdvice(faceRects, image.cols);
 
 }
 
-centralizePicture('samples/2018.jpg');
+centralizePicture('samples/2014.png');
 
-
-/*
-// import nodejs bindings to native tensorflow,
-// not required, but will speed up things drastically (python required)
-//import '@tensorflow/tfjs-node';
-import * as canvas from 'canvas';
-import * as faceapi from 'face-api.js';
-import { readFile } from 'fs'
-import { tf, toNetInput } from 'face-api.js';
-// implements nodejs wrappers for HTMLCanvasElement, HTMLImageElement, ImageData
-
-function toArrayBuffer(buf) {
-    var ab = new ArrayBuffer(buf.length);
-    var view = new Uint8Array(ab);
-    for (var i = 0; i < buf.length; ++i) {
-        view[i] = buf[i];
-    }
-    return ab;
-}
-
-
-
- /*
-    // patch nodejs environment, we need to provide an implementation of
-    // HTMLCanvasElement and HTMLImageElement
-    
-    const { Canvas, Image, ImageData } = canvas;
-    const x = faceapi.env.monkeyPatch({ /*Canvas, Image, ImageData })
-    
-
-    readFile(name, function (err, data) {
-        if (err) throw err;
-
-        const img = toArrayBuffer(data)
-
-        const netInput = img instanceof tf.Tensor
-      ? img
-      : await toNetInput(data)
-
-        const detection = faceapi.detectSingleFace(img as Float32Array) //What is TNetInput ????
-        console.log(detection);
-        //res.write(data);
-    }); */
---
+++ src/Typescript example.ts
@@ -0,0 +1,34 @@
+import * as fr from 'face-recognition'
+
+class FaceRecognitionProcessor {
+
+public async toyImage(name: string) {
+
+    
+    const predictor = fr.FaceLandmark5Predictor();
+
+    const image = fr.loadImage(name);
+
+    console.log('Image loaded');
+    const detector = fr.FaceDetector();
+    const targetSize = 640;
+
+    const faceImages = detector.detectFaces(image, targetSize);
+    console.log('face detected');
+    const faceRects = detector.locateFaces(image)
+
+    //const shapes = faceRects.map(rect => predictor.predict(image, rect));
+    console.log(faceRects);
+    
+
+    // save file
+    faceImages.forEach((img, i) => fr.saveImage(`../outputs/face_${i}.png`, img))
+
+}
+
+
+
+}
+
+
+export default new FaceRecognitionProcessor();
